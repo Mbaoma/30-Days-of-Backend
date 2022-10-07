9c2dbf8ff2e9a92c8063633793027243ea460d04
Mbaoma <mmaryraphaella@gmail.com>
2022-10-07 21:35:42 +0100

    add check for whitespace

--- day1.java
+++ day1.java
@@ -1,28 +1,27 @@
 //BoO(“Micheal Jackson”)=== “Please remove all spaces or special characters”
 //BoO(“Micheal”)=== “It is nice to meet you, Micheal”;
 //BoO(“Bolu”)=== “Welcome back, Bolu”;
+
 import java.util.*;  
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class singleString{
     public static void main(String[] args){
-        Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+        Scanner sc= new Scanner(System.in);   
         System.out.print("Enter a string: ");
-        String user_input= sc.nextLine(); //reads string 
-        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        String user_input= sc.nextLine();  
+        Pattern p = Pattern.compile("[^a-z0-9'']", Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(user_input);
         boolean res = m.find();
-  
-        //String regex = "[!-\\/:-@[-`\\{-~\\s\\g]";
-         
+   
         if(res){
             System.out.println("Please remove all spaces or special characters");
         }
         else if (user_input == "Bolu" || user_input == "Odun" ){
             System.out.println("Welcome back, " +user_input);
         } else {
-            System.out.println("Welcome back2, " +user_input); 
+            System.out.println("It is nice to meet you, " +user_input); 
         }
     }
 }
